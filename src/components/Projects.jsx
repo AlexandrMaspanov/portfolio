7597989e05ac4993cab1256626ff4070033f450e
Alexandr Maspanov <alexandrmaspanov@gmail.com>
2025-05-14 22:25:07 +0300

    feat: add loader component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import ProjectCard from "./projectCard/ProjectCard";
+import Loader from "./UI/loader/Loader";
 
 const Projects = () => {
     const username = 'alexandrmaspanov';
     const [repos, setRepos] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         fetch(`https://api.github.com/users/${username}/repos`)
             .then(response => response.json())
             .then(data => {
@@ -14,24 +17,30 @@ const Projects = () => {
                 );
                 setRepos(filtered);
             })
-            .catch(err => console.error(err));
+            .catch(err => console.error(err))
+            .finally(() => setIsLoading(false));
     }, []);
 
     return (
         <section className="projects">
             <h2>My Projects</h2>
             <div className="projectsGrid">
-                {
-                    repos.map(repo =>
-                        <ProjectCard
-                            key={repo.id}
-                            name={repo.name}
-                            fullName={repo.full_name}
-                            description={repo.description}
-                            topics={repo.topics}
-                            link={repo.html_url}
-                            homepage={repo.homepage}
-                        />
+                {isLoading
+                ? <Loader />
+                : (
+                    repos.length
+                        ? repos.map(repo =>
+                            <ProjectCard
+                                key={repo.id}
+                                name={repo.name}
+                                fullName={repo.full_name}
+                                description={repo.description}
+                                topics={repo.topics}
+                                link={repo.html_url}
+                                homepage={repo.homepage}
+                            />
+                        )
+                        : <div className="notFound">Projects not found!</div>
                     )
                 }
             </div>
